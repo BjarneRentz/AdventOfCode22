71d8558fe5a6c72287f93d4f78a532840b561337
Bjarne Rentz <Bjarnerentz@live.de>
2022-12-14 21:42:32 +0100

    Initial commit

---
+++ Challenges/Challenge1.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode22.Challenges;
+
+public class Challenge1
+{
+	public void Solve(string rawInput)
+	{
+		List<int> caloriesPerElf = new();
+
+		var numbers = rawInput.Split("\n");
+
+		var currentPos = 0;
+		foreach (var number in numbers)
+		{
+			if (number == "")
+			{
+				currentPos++;
+				continue;
+			}
+			
+			if(caloriesPerElf.Count == currentPos)
+				caloriesPerElf.Insert(currentPos,Convert.ToInt32(number));
+			else
+				caloriesPerElf[currentPos] += Convert.ToInt32(number);
+		}
+		
+		Console.WriteLine(caloriesPerElf.Max());
+	} 
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode22.Challenges;
+
+Console.WriteLine("Hello, World!");
+
+var cookie = Environment.GetEnvironmentVariable("AOC_COOKIE");
+
+using var httpClient = new HttpClient();
+
+var message = new HttpRequestMessage();
+message.Headers.Add("Cookie", cookie);
+message.Method  = HttpMethod.Get;
+message.RequestUri = new Uri("https://adventofcode.com/2022/day/1/input");
+
+var result = await httpClient.SendAsync(message);
+var challengeInput = await result.Content.ReadAsStringAsync();
+
+var challenge = new Challenge1();
+
+challenge.Solve(challengeInput);
